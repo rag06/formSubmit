ea5235f7fb98d8b1daeee2c83210410656ec1e72
Anurag Singh <singhanuragv@gmail.com>
2018-06-21 21:03:15 +0530

    added js plugin

--- formSubmit.js
+++ formSubmit.js
@@ -1,40 +1,28 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<script>
 (function($) {
 
     $.fn.enquiryFormSubmission = function(options ) {
 
          // Establish our default settings
         var settings = $.extend({
-             name:'name',
-             email:'email',
-             message:'message',
+             keys:['name','email','message'],
              form:this,
+             url:'http://127.0.0.1/anuragform.php',
              companyId:null
         }, options);
 
         this.find('input[type="submit"],button[type="submit"] ').click(function(event){
             event.preventDefault();
-            console.log(settings);
-            var formDataArray = $(settings.form).serialize();
-            var dataSubmit = {
-                  name :formDataArray[settings.name],
-                  email :formDataArray[settings.email],
-                  message :formDataArray[settings.message],
-            };
-            dataSubmit =JSON.parse(JSON.stringify(formDataArray));
+            var formDataArray =[];
+            formDataArray = $(settings.form).serializeArray();
+            console.log(formDataArray);
+            formDataArray.push({name:"keys",value:settings.keys});
+            formDataArray.push({name:"companyId",value:settings.companyId});
+
             $.ajax({
                  type : 'POST',
-                 url : 'http://127.0.0.1/anuragform.php',
-                 data : dataSubmit
+                 url : settings.url,
+                 dataType : 'json',
+                 data :  JSON.stringify(formDataArray)
             });
         });
     }
@@ -42,62 +30,3 @@
 
 
 }(jQuery));
-</script>
-<div class="container">
-	<div class="row">
-
-	<form  class="form-horizontal">
-        <fieldset>
-
-        <!-- Form Name -->
-        <legend>Form Name</legend>
-
-        <!-- Text input-->
-        <div class="form-group">
-          <label class="col-md-12 control-label" for="name">Name</label>
-          <div class="col-md-12">
-          <input id="name" name="name" type="text" placeholder="Name" class="form-control input-md" value="anurag" required="">
-          <span class="help-block">Name</span>
-          </div>
-        </div>
-
-        <!-- Text input-->
-        <div class="form-group">
-          <label class="col-md-12 control-label" for="email">Email</label>
-          <div class="col-md-12">
-          <input id="email" name="email" type="text" placeholder="Email" class="form-control input-md"   value="anurag@zycus.com"  required="">
-          <span class="help-block">Email</span>
-          </div>
-        </div>
-
-        <!-- Textarea -->
-        <div class="form-group">
-          <label class="col-md-12 control-label" for="message">Message</label>
-          <div class="col-md-12">
-            <textarea class="form-control" id="message" name="message">Anurag Message</textarea>
-          </div>
-        </div>
-        <!-- Button (Double) -->
-        <div class="form-group">
-          <label class="col-md-12 control-label" for="submit"></label>
-          <div class="col-md-12">
-            <button id="submit" name="submit" type="submit" class="btn btn-success">Submit</button>
-            <button id="reset" name="reset" type="reset" class="btn btn-danger">Reset</button>
-          </div>
-        </div>
-
-        </fieldset>
-        </form>
-
-	</div>
-</div>
-<script>
-$( document ).ready(function() {
-    $( 'form' ).enquiryFormSubmission({
-        name:'name',
-        email:'email',
-        message:'message',
-        companyId: 2500
-    });
-});
-</script>
